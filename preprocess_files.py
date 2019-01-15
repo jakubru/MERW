ff80c32b1eeb97211e56f1753d830298c59a86c4
Konrad <konrad.korus0@gmail.com>
2019-01-15 18:32:35 +0100

    preprocessing pliku (usuniecie duplikatow i przenumerowanie numerow wierzcholkow)

---
+++ preprocess_files.py
@@ -0,0 +1,31 @@
+import pandas
+import os
+
+
+def preprocess_file(path):
+    df = pandas.read_csv(path, sep=' ', header=None)
+    df.columns = ['from', 'to']
+    df.sort_values(by=['from', 'to'], inplace=True)
+
+    df2 = df.copy()
+    df2['from'], df2['to'] = df['to'], df['from']
+    all_edges = df.append(pandas.DataFrame(df2)).drop_duplicates()
+
+    max_index = df.max() - df.min()
+    indexes = list(range(max_index.max()))
+    index = 0
+    prev = -1
+    for vertex in all_edges['from']:
+        if vertex != prev:
+            all_edges.replace(vertex, indexes[index], inplace=True)
+            index += 1
+        prev = vertex
+    return all_edges
+
+
+if __name__ == '__main__':
+    root = 'facebook'
+    for path in os.listdir(root):
+        if '.edges' in path:
+            print(preprocess_file(os.path.join(root, path)))
+            print()
