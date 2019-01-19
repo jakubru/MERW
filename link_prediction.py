5607129b294f6118eaed31bc13f5b43fcb77c431
Konrad <konrad.korus0@gmail.com>
2019-01-19 13:13:23 +0100

    Renamed functions in link prediction.py

--- link_prediction.py
+++ link_prediction.py
@@ -4,6 +4,7 @@ import laplacians
 import inverse_p_distance
 import sim_rank
 
+
 class LinkPrediction:
 
     def __init__(self, graph, approach='MERW', method='laplacians', **kwargs):
@@ -12,72 +13,72 @@ class LinkPrediction:
         self.graph = graph
         self.kwargs = kwargs
 
-    def fit(self, edges_percent=0.1):
+    def pred(self, edges_percent=0.1):
         edges_idx = np.argwhere(self.graph > 0)
         edges_to_delete = np.random.randint(0, len(edges_idx), int(edges_percent * len(edges_idx)))
-        graph_removed_edges = a.copy()
+        graph_removed_edges = self.graph.copy()
         removed_indices = edges_idx[edges_to_delete]
         graph_removed_edges[removed_indices] = 0
         if self.approach == 'MERW':
-            preds = self._fit_merw(graph_removed_edges, removed_indices)
+            preds = self._pred_merw(graph_removed_edges, removed_indices)
         elif self.approach == 'TRW':
-            preds = self._fit_trw(graph_removed_edges, removed_indices)
+            preds = self._pred_trw(graph_removed_edges, removed_indices)
         else:
             raise RuntimeError("Link prediction approach must be set to 'MERW' or 'TRW'")
         score = self.score(preds, removed_indices)
         return preds, score
 
-    def _fit_merw(self, graph_removed_edges, removed_indices):
+    def _pred_merw(self, graph_removed_edges, removed_indices):
         if self.method == 'laplacians':
-            return self._fit_laplacians_merw(graph_removed_edges, removed_indices)
+            return self._pred_laplacians_merw(graph_removed_edges, removed_indices)
         elif self.method == 'simrank':
-            return self._fit_simrank_merw(graph_removed_edges, removed_indices)
+            return self._pred_simrank_merw(graph_removed_edges, removed_indices)
         elif self.method == 'inv_p_dist':
-            return self._fit_inv_p_dist_merw(graph_removed_edges, removed_indices)
+            return self._pred_inv_p_dist_merw(graph_removed_edges, removed_indices)
         else:
             raise RuntimeError("Link prediction method must be set to 'laplacians', 'simrank' or 'inv_p_dist'")
 
-    def _fit_trw(self, graph_removed_edges, removed_indices):
+    def _pred_trw(self, graph_removed_edges, removed_indices):
         if self.method == 'laplacians':
-            return self._fit_laplacians_trw(graph_removed_edges, removed_indices)
+            return self._pred_laplacians_trw(graph_removed_edges, removed_indices)
         elif self.method == 'simrank':
-            return self._fit_simrank_trw(graph_removed_edges, removed_indices)
+            return self._pred_simrank_trw(graph_removed_edges, removed_indices)
         elif self.method == 'inv_p_dist':
-            return self._fit_inv_p_dist_trw(graph_removed_edges, removed_indices)
+            return self._pred_inv_p_dist_trw(graph_removed_edges, removed_indices)
         else:
             raise RuntimeError("Link prediction method must be set to 'laplacians', 'simrank' or 'inv_p_dist'")
 
-    def _fit_laplacians_trw(self, graph_removed_edges, removed_indices):
+    def _pred_laplacians_trw(self, graph_removed_edges, removed_indices):
         n_preds = len(removed_indices)
         preds = ...
         new_edges = _largest_indices(preds, n_preds)
         return new_edges
 
-    def _fit_laplacians_merw(self, graph_removed_edges, removed_indices):
+    def _pred_laplacians_merw(self, graph_removed_edges, removed_indices):
         n_preds = len(removed_indices)
         preds = ...
         new_edges = _largest_indices(preds, n_preds)
         return new_edges
 
-    def _fit_simrank_trw(self, graph_removed_edges, removed_indices):
+    def _pred_simrank_trw(self, graph_removed_edges, removed_indices):
         n_preds = len(removed_indices)
         preds = ...
         new_edges = _largest_indices(preds, n_preds)
         return new_edges
 
-    def _fit_simrank_merw(self, graph_removed_edges, removed_indices):
+    def _pred_simrank_merw(self, graph_removed_edges, removed_indices):
         n_preds = len(removed_indices)
         preds = ...
         new_edges = _largest_indices(preds, n_preds)
         return new_edges
 
-    def _fit_inv_p_dist_trw(self, graph_removed_edges, removed_indices):
+    def _pred_inv_p_dist_trw(self, graph_removed_edges, removed_indices):
         n_preds = len(removed_indices)
         preds = ...
         new_edges = _largest_indices(preds, n_preds)
         return new_edges
 
-    def _fit_inv_p_dist_merw(self, graph_removed_edges, removed_indices):
+    def _pred_inv_p_dist_merw(self, graph_removed_edges, removed_indices):
         n_preds = len(removed_indices)
         preds = ...
         new_edges = _largest_indices(preds, n_preds)
@@ -93,6 +94,3 @@ def _largest_indices(ary, n):
     indices = np.argpartition(flat, -n)[-n:]
     indices = indices[np.argsort(-flat[indices])]
     return np.unravel_index(indices, ary.shape)
-
-a = np.array([[0, 1], [1, 3]])
-print(np.argpartition(a, -4)[-4:])
