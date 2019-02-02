cd5ef8101a4a603280db257b756ebc0767761e18
SteelHawX <bart.gniady@gmai.com>
2019-02-02 13:38:42 +0100

    poprawione usuwanie krawedzi

--- link_prediction.py
+++ link_prediction.py
@@ -25,12 +25,16 @@ class LinkPrediction:
             sum_row = np.sum(self.graph, axis=1)
             sum_col = np.sum(self.graph, axis=0)
             actual_edges = []
+            print(sum_row)
             print(f'Original graph det: {np.linalg.det(self.graph)}')
             for i, j in removed_indices:
                 if sum_row[i] > 1 and sum_col[j] > 1:
                     graph_removed_edges[i, j] = 0
                     graph_removed_edges[j, i] = 0
                     actual_edges.append([i, j])
+                    sum_row[i] -= 1
+                    sum_col[j] -= 1
+            print(sum_row)
             actual_edges = np.array(actual_edges)
             print(f'Removed {len(actual_edges)/len(edges_idx)} edges')
             no_edges_idx = np.argwhere(self.graph == 0)
